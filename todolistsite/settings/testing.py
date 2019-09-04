9688353261ed4dfeed4fb1e97bd4864f2420304a
nicolas.nunez <nicolas.nunez@eventbrite.com>
2019-09-04 14:34:55 -0300

    Settings modified

---
+++ todolistsite/settings/testing.py
@@ -0,0 +1,15 @@
+from .base import *  # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    'testserver'
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
